6791ec9e75a80b49baa550cb04fe887feff592e4
sumitradas94 <sumitradas94@gmail.com>
2023-01-10 21:20:43 +0530

    Find maximum of Integer

---
+++ MaxInteger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class MaxInteger<T> where T : IComparable
+    {
+        public static T MaxIntNumber(T X, T Y, T Z)
+        {
+            if (X.CompareTo(Y) > 0 && X.CompareTo(Z) > 0 ||
+                X.CompareTo(Y) >= 0 && X.CompareTo(Z) > 0 ||
+                X.CompareTo(Y) > 0 && X.CompareTo(Z) >= 0)
+
+            {
+                return X;
+            }
+
+            if (Y.CompareTo(X) > 0 && Y.CompareTo(Z) > 0 ||
+                Y.CompareTo(X) >= 0 && Y.CompareTo(Z) > 0 ||
+                Y.CompareTo(X) > 0 && Y.CompareTo(Z) >= 0)
+
+            {
+                return Y;
+            }
+
+            if (Z.CompareTo(X) > 0 && Z.CompareTo(Y) > 0 ||
+                Z.CompareTo(X) >= 0 && Z.CompareTo(Y) > 0 ||
+                Z.CompareTo(X) > 0 && Z.CompareTo(Y) >= 0)
+
+            {
+                return Z;
+            }
+            return X;
+        }
+    }
+}
