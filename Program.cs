7b1e532076586f1b395e23ce0e6ac299c0b9534d
sumitradas94 <sumitradas94@gmail.com>
2023-01-10 21:26:29 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Find Maximum Problem using Generics");
+            //Console.WriteLine("Welcome to Find Maximum Problem using Generics");
+            int X = 20;
+            int Y = 400;
+            int Z = 6000;
+
+            int val = MaxInteger<int>.MaxIntNumber(X, Y, Z);
+            Console.WriteLine("The greater num is " + val);
         }
     }
 }
