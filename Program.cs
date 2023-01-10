138c494536f654d4afa01fc3ed49c79e43479768
sumitradas94 <sumitradas94@gmail.com>
2023-01-10 21:10:25 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMaximum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Problem using Generics");
+        }
+    }
+}
