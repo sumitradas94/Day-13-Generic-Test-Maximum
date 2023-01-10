1b02b4152a7cac00e5732a7db24c1841c4db38af
sumitradas94 <sumitradas94@gmail.com>
2023-01-10 21:54:31 +0530

    Method calling for UC3

--- Program.cs
+++ Program.cs
@@ -9,15 +9,22 @@
             //int Y = 400;
             // int Z = 6000;
 
-            //int val = MaxInteger<int>.MaxIntNumber(X, Y, Z);
-            //Console.WriteLine("The greater num is " + val);
+//int val = MaxInteger<int>.MaxIntNumber(X, Y, Z);
+//Console.WriteLine("The greater num is " + val);
 
-            float A = 40.5F;
-            float B = 50.6F;
-            float C = 80.9F;
+//float A = 40.5F;
+// float B = 50.6F;
+ //float C = 80.9F;
 
-            float val = MaxFloat<float>.MaxFloatNum(A, B, C);
-            Console.WriteLine("The greater Float Num  is " + val);
-        }
-    }
+ //float val = MaxFloat<float>.MaxFloatNum(A, B, C);
+ //Console.WriteLine("The greater Float Num  is " + val);
+
+  string L = "Red";
+ string M = "Yellow";
+ string N = "Purple";
+
+ string val2 = MaxString<string>.MaxStringNum(L, M, N);
+ Console.WriteLine("The greater String is " + val2);
+}
+}
 }
