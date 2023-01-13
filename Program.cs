20efd8aa9ffae556c5d882a0971caaadae1d3018
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 14:29:10 +0530

    modifying for uc4

--- Program.cs
+++ Program.cs
@@ -9,22 +9,25 @@
             //int Y = 400;
             // int Z = 6000;
 
-//int val = MaxInteger<int>.MaxIntNumber(X, Y, Z);
-//Console.WriteLine("The greater num is " + val);
+            //int val = MaxInteger<int>.MaxIntNumber(X, Y, Z);
+            //Console.WriteLine("The greater num is " + val);
 
-//float A = 40.5F;
-// float B = 50.6F;
- //float C = 80.9F;
+            //float A = 40.5F;
+            // float B = 50.6F;
+            //float C = 80.9F;
 
- //float val = MaxFloat<float>.MaxFloatNum(A, B, C);
- //Console.WriteLine("The greater Float Num  is " + val);
+            //float val = MaxFloat<float>.MaxFloatNum(A, B, C);
+            //Console.WriteLine("The greater Float Num  is " + val);
 
-  string L = "Red";
- string M = "Yellow";
- string N = "Purple";
+            //string L = "Red";
+            // string M = "Yellow";
+            // string N = "Purple";
 
- string val2 = MaxString<string>.MaxStringNum(L, M, N);
- Console.WriteLine("The greater String is " + val2);
-}
+            // string val2 = MaxString<string>.MaxStringNum(L, M, N);
+            //Console.WriteLine("The greater String is " + val2);
+
+            Max3Parameter<int> obj = new Max3Parameter<int>(66, 99, 92);
+            obj.MaxMethod();
+        }
 }
 }
