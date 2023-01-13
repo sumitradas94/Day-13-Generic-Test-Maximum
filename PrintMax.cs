a67f3bb264cef1c20585fb0e9670806ae856a137
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 14:41:10 +0530

    print max to std out

---
+++ PrintMax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class PrintMax<T> where T : IComparable
+    {
+        public T A, B, C, Max;
+        public PrintMax(T A, T B, T C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+        public static T MaxNum(T A, T B, T C)
+        {
+            if (A.CompareTo(B) > 0 && A.CompareTo(C) > 0 ||
+                    A.CompareTo(B) >= 0 && A.CompareTo(C) > 0 ||
+                    A.CompareTo(B) > 0 && A.CompareTo(C) >= 0)
+            {
+                return A;
+            }
+
+            if (B.CompareTo(A) > 0 && B.CompareTo(C) > 0 ||
+                    B.CompareTo(A) >= 0 && B.CompareTo(C) > 0 ||
+                    B.CompareTo(B) > 0 && B.CompareTo(C) >= 0)
+            {
+                return B;
+            }
+
+            if (C.CompareTo(B) > 0 && C.CompareTo(A) > 0 ||
+                    C.CompareTo(B) >= 0 && C.CompareTo(A) > 0 ||
+                    C.CompareTo(B) > 0 && C.CompareTo(A) >= 0)
+            {
+                return C;
+            }
+            return default;
+        }
+        public T MaxMethod()
+        {
+            Max = PrintMax<T>.MaxNum(this.A, this.B, this.C);
+            toPrint(Max);
+            return Max;
+
+        }
+        public void toPrint(T Max)
+        {
+            Console.WriteLine("Maximum is " + Max);
+        }
+}
